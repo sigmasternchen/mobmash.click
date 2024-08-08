ad8391177526d1d777ca82697b5e18585f9202e5
overflowerror <mail@overflowerror.com>
2024-08-08 20:49:07 +0200

    cleanup: Add newlines to cron job output

--- bin/cron/housekeeping.php
+++ bin/cron/housekeeping.php
@@ -3,10 +3,10 @@
 require_once __DIR__ . "/../../core.php";
 require_once __DIR__ . "/../../lib/housekeeping.php";
 
-echo "Removing old session IDs from match table...";
+echo "Removing old session IDs from match table...\n";
 removeOldSessionIDs();
 
-echo "Removing old audit logs...";
+echo "Removing old audit logs...\n";
 removeOldAuditLogs();
 
-echo "Housekeeping done.";
+echo "Housekeeping done.\n";
--- bin/cron/updateCache.php
+++ bin/cron/updateCache.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/../../core.php";
 require_once __DIR__ . "/../../lib/updateCache.php";
 
-echo "Updating rating cache...";
+echo "Updating rating cache...\n";
 updateCache();
 
-echo "Done.";
+echo "Done.\n";
